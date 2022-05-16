79f6877f23ea6017a918b93de64b68133de618bf
Julian Porter <julian@porternet.org.uk>
2022-05-17 00:22:12 +0100

    We have DPI

--- MyPlayground2.playground/Contents.swift
+++ MyPlayground2.playground/Contents.swift
@@ -1,170 +1,125 @@
 import Foundation
 import AppKit
 import UniformTypeIdentifiers
+import ImageIO
 
-enum PrinterError : Error {
-    case GeneralError(OSStatus)
-    case PointerError
-    case PrinterNameError
-    case PrinterIDError
-    
-    static func wrap(_ e : OSStatus) throws {
-        guard e==0 else { throw PrinterError.GeneralError(e) }
-    }
+func sourceTypes() -> [String] {
+    let a = CGImageSourceCopyTypeIdentifiers()
+    return a as! Array<String>
+}
+func destinationTypes() -> [String] {
+    let a = CGImageDestinationCopyTypeIdentifiers()
+    return a as! Array<String>
 }
 
 
-class PrinterInfo : Sequence,CustomStringConvertible  {
-    typealias Iterator=Array<PMResolution>.Iterator
-    let name : String
-    let id : String
-    let model : String?
-    let mimeTypes : [String]
-    let resolutions : [PMResolution]
-    let printer : PMPrinter
-    
-    init(_ printer: PMPrinter) throws {
-        self.printer=printer
-        
-        let n : Unmanaged<CFString>? = PMPrinterGetName(printer)
-        guard let nn = n?.takeUnretainedValue() as String? else { throw PrinterError.PrinterNameError }
-        self.name=nn
-        
-        let i : Unmanaged<CFString>? = PMPrinterGetID(printer)
-        guard let ii = i?.takeUnretainedValue() as String? else { throw PrinterError.PrinterIDError }
-        self.id=ii
-        
-        var m : Unmanaged<CFString>?
-        try PrinterError.wrap(PMPrinterGetMakeAndModelName(printer, &m))
-        model = m?.takeUnretainedValue() as String?
-        
-        var mi : Unmanaged<CFArray>?
-        try PrinterError.wrap(PMPrinterGetMimeTypes(printer, nil, &mi))
-        if let mRet = mi?.takeUnretainedValue() {
-            let cfs = mRet as! Array<CFString>
-            mimeTypes = cfs.map { $0 as String }
-        }
-        else { mimeTypes=[] }
-        
-        var nr : UInt32 = 0
-        try PrinterError.wrap(PMPrinterGetPrinterResolutionCount(printer,&nr))
-        let range = 1...nr
-        self.resolutions = range.compactMap { idx in
-            var res = PMResolution()
-            let e = PMPrinterGetIndexedPrinterResolution(printer, idx, &res)
-            guard e==0 else { return nil }
-            return res
-        }
-    }
-    
-    var description: String { "Name : \(name) ID: \(id) Model: \(model ?? "")" }
-    
-    var count : Int { resolutions.count }
-    func makeIterator() -> Array<PMResolution>.Iterator { resolutions.makeIterator() }
-    
-    
-    var isDefault : Bool { PMPrinterIsDefault(self.printer) }
-    func mimeTypeIsSupported(_ mime: String) -> Bool { self.mimeTypes.contains(mime) }
-    
-    private var session: PMPrintSession?
-    private var settings : PMPrintSettings?
-    
-    private func prepare() throws {
-        try PrinterError.wrap(PMCreateSession(&session))
-        try PrinterError.wrap(PMSessionSetCurrentPMPrinter(session!, self.printer))
-        
-        try PrinterError.wrap(PMCreatePrintSettings(&settings))
-        try PrinterError.wrap(PMSessionDefaultPrintSettings(session!, settings!))
-    }
-    
-    private func setResolution(_ resolution : PMResolution) throws {
-        var res=resolution
-        try PrinterError.wrap(PMPrinterSetOutputResolution(self.printer, settings!, &res))
-    }
-    
-    private func release() {
-        PMRelease(&settings)
-        settings=nil
-        PMRelease(&session)
-        session=nil
-    }
-    
-    func print(data: Data, resolution: PMResolution,mime: String = "image/png") throws {
-        guard let prov = CGDataProvider(data: data as CFData) else { return }
-        
-        try self.prepare()
-        try self.setResolution(resolution)
-        try PrinterError.wrap(PMPrinterPrintWithProvider(self.printer, settings!, nil, mime as CFString, prov))
-        self.release()
-        
-    }
-    func print(file: URL, resolution: PMResolution,mime: String = "image/png") throws {
-        
-         var session: PMPrintSession?
-         var settings : PMPrintSettings?
-        
-        try PrinterError.wrap(PMCreateSession(&session))
-        try PrinterError.wrap(PMSessionSetCurrentPMPrinter(session!, self.printer))
-        
-        try PrinterError.wrap(PMCreatePrintSettings(&settings))
-        try PrinterError.wrap(PMSessionDefaultPrintSettings(session!, settings!))
-        
-        //try self.prepare()
-        var res=resolution
-        try PrinterError.wrap(PMPrinterSetOutputResolution(self.printer, settings!, &res))
-        try PrinterError.wrap(PMPrinterPrintWithFile(self.printer, settings!, nil, mime as CFString, file as CFURL))
-        //self.release()
-    }
-    
-    
+
+
+enum KError : Error {
+    case Src
+    case Img
+    case Prop
+    case Ty
+    case Dst
 }
 
+let CGImagePropertyDPIWidth = kCGImagePropertyDPIWidth as String
+let CGImagePropertyDPIHeight = kCGImagePropertyDPIHeight as String
+let CGImagePropertyTIFFDictionary = kCGImagePropertyTIFFDictionary as String
+let CGImagePropertyTIFFXResolution = kCGImagePropertyTIFFXResolution as String
+let CGImagePropertyTIFFYResolution = kCGImagePropertyTIFFYResolution as String
 
-class PrintSystem : Sequence {
-    public typealias Iterator=Array<PrinterInfo>.Iterator
-    
-    public private(set) var printers : [PrinterInfo] = []
-    
-    public init() throws {
-        var p : Unmanaged<CFArray>?
-        try PrinterError.wrap(PMServerCreatePrinterList(nil, &p))
-        if let ps = p?.takeUnretainedValue()
-        {
-            let range=0..<(CFArrayGetCount(ps))
-            let printers : [PMPrinter] = range.compactMap { i in PMPrinter(CFArrayGetValueAtIndex(ps, i)) }
-            self.printers = try printers.map { try PrinterInfo($0) }
-        }
-        else { self.printers = [] }
-    }
-    
-    public var defaultPrinter : PrinterInfo? { printers.first { $0.isDefault }}
-    
-    public var count : Int { printers.count }
-    func makeIterator() -> Iterator { printers.makeIterator() }
-    
-    subscript(_ id : String) -> PrinterInfo? { printers.first { $0.id == id } }
-    var def : PrinterInfo? { printers.first { $0.isDefault } }
-    
-    func show() { printers.forEach { print($0.description) } }
-    
-  
-        
-}
-
-
+let CGImagePropertyJFIFDictionary = kCGImagePropertyJFIFDictionary as String
+let CGImagePropertyJFIFXDensity = kCGImagePropertyJFIFXDensity as String
+let CGImagePropertyJFIFYDensity = kCGImagePropertyJFIFYDensity as String
+let CGImagePropertyJFIFVersion = kCGImagePropertyJFIFVersion as String
 
 do {
-    let pr=try PrintSystem()
-    pr.show()
-    let p = pr.defaultPrinter!
-    p.resolutions.forEach { print($0) }
-    let res=p.resolutions[0]
-    let u=URL(fileURLWithPath: "/Users/julianporter/Pictures/Excel/78691.jpg")
-    try p.print(file: u, resolution: res, mime: "image/jpg")
+    let path = NSURL(fileURLWithPath: "/Users/julianporter/Pictures/Excel/Ropponmatsu_II_244.jpg")
+    
+    let info = [
+        kCGImageSourceShouldCache : true,
+        kCGImageSourceShouldAllowFloat : true
+    ]
+    guard let src = CGImageSourceCreateWithURL(path as CFURL, info as CFDictionary) else { throw KError.Src }
+    guard let image = CGImageSourceCreateImageAtIndex(src, 0, nil) else { throw KError.Img }
+    guard let ky = CGImageSourceCopyPropertiesAtIndex(src,0, nil) else { throw KError.Prop }
+    
+    guard let ty = CGImageSourceGetType(src) else { throw KError.Ty }
+    let dpi : Int = 300
+    var keys = ky as? [String:Any] ?? [String:Any]()
+    keys[CGImagePropertyDPIWidth] = dpi
+    keys[CGImagePropertyDPIHeight] = dpi
+    
+    var exifDictionary = keys[CGImagePropertyTIFFDictionary] as? [String:Any] ?? [String:Any]()
+    exifDictionary[CGImagePropertyTIFFXResolution] = dpi
+    exifDictionary[CGImagePropertyTIFFYResolution] = dpi
+    keys[CGImagePropertyTIFFDictionary] = exifDictionary
+
+    var jfifDictionary = keys[CGImagePropertyJFIFDictionary] as? [String:Any] ?? [String:Any]()
+    jfifDictionary[CGImagePropertyJFIFXDensity] = dpi
+    jfifDictionary[CGImagePropertyJFIFYDensity] = dpi
+    jfifDictionary[CGImagePropertyJFIFVersion ] = 1
+    keys[CGImagePropertyJFIFDictionary] = jfifDictionary
+
+    
+    let path2 = NSURL(fileURLWithPath: "/Users/julianporter/Pictures/Excel/Ropponmatsu_II_301.jpg")
+    guard let dest = CGImageDestinationCreateWithURL(path2 as CFURL, ty, 1, keys as CFDictionary?) else { throw KError.Dst }
+    CGImageDestinationAddImage(dest, image, keys as CFDictionary?)
+    CGImageDestinationFinalize(dest)
 }
-catch(let e) { print(e) }
+catch (let e) { print(e) }
+
+/*
+ Image property dictionaries
+ EXIF -> JPG and TIF and some PNGs
+ TIFF -> Same as EXIF
+ JFIF -> some JPG
+ PMG  -> PNG
+ 
+ For PNG, minimum is
+ DPIWidth
+ DPIHeight
+ PixelWidth
+ PixelHeight
+ ColorModel = RGB
+ HasAlpha = 0 or 1
+ Depth
+ PNG {
+    InterlaceType = 0
+    XPixelsPerMetre
+    YPixelsPerMetry
+ }
+ 
+ */
+
+func listProps(path p: String) throws {
+    let path = NSURL(fileURLWithPath: p)
     
+    let info = [
+        kCGImageSourceShouldCache : true,
+        kCGImageSourceShouldAllowFloat : true
+    ]
+    guard let src = CGImageSourceCreateWithURL(path as CFURL, info as CFDictionary) else { throw KError.Src }
+    //guard let image = CGImageSourceCreateImageAtIndex(src, 0, nil) else { throw KError.Img }
+    guard let ky = CGImageSourceCopyPropertiesAtIndex(src,0, nil) else { throw KError.Prop }
     
+    guard let ty = CGImageSourceGetType(src) else { throw KError.Ty }
+    let keys = ky as? [String:Any] ?? [String:Any]()
+    
+    print(p)
+    print("Type is \(ty)")
+    keys.forEach { print("\($0.key) == \($0.value)")}
+}
+
+do {
+    try listProps(path:"/Users/julianporter/Pictures/portia.png")
+    try listProps(path:"/Users/julianporter/Pictures/midi.png")
+    try listProps(path:"/Users/julianporter/Pictures/me.png")
+    
+}
+
+
     
 
 
--- lace/Printing/pdf.swift
+++ lace/Printing/pdf.swift
@@ -6,23 +6,72 @@
 //
 
 import Foundation
-import PDFKit
+import ImageIO
 
-class Delegate : NSObject, PDFDocumentDelegate {
+enum Keys {
+    case DPIWidth
+    case DPIHeight
+    case TIFFDictionary
+    case TIFFXResolution
+    case TIFFYResolution
+    case JFIFDictionary
+    case JFIFXDensity
+    case JFIFYDensity
+    case JFIFVersion
+    case PNGXResolution
+    case PNGYResolution
+    case PNGDictionary
     
-    func classForPage() -> AnyClass { ImagePage.self }
+    static let values : [Keys:CFString] = [
+        .DPIWidth : kCGImagePropertyDPIWidth,
+        .DPIHeight : kCGImagePropertyDPIHeight,
+        .TIFFDictionary : kCGImagePropertyTIFFDictionary,
+        .TIFFXResolution : kCGImagePropertyTIFFXResolution,
+        .TIFFYResolution : kCGImagePropertyTIFFYResolution,
+        .JFIFDictionary : kCGImagePropertyJFIFDictionary,
+        .JFIFVersion : kCGImagePropertyJFIFVersion,
+        .JFIFXDensity : kCGImagePropertyJFIFXDensity,
+        .JFIFVersion : kCGImagePropertyJFIFYDensity,
+        .PNGXResolution : kCGImagePropertyPNGXPixelsPerMeter,
+        .PNGYResolution : kCGImagePropertyPNGYPixelsPerMeter,
+        .PNGDictionary : kCGImagePropertyPNGDictionary
+    ]
+    
+    var cf : CFString { Keys.values[self]! }
+    var str : String { self.cf as String }
 }
 
-class ImagePage : PDFPage {
+typealias DataDict = [String:Any]
+
+
+func asDataDict(_ dict : Any?) -> DataDict { (dict as? DataDict) ?? DataDict() }
+
+
+struct Metadata {
+    var metadata : DataDict = [:]
     
-    override func draw(with box: PDFDisplayBox, to context: CGContext) {
-        super.draw(with: box, to: context)
-        
-        context.saveGState()
-        
-        context.draw(<#T##image: CGImage##CGImage#>, in: <#T##CGRect#>)
-        
-        context.restoreGState()
+    init(_ dict : CFDictionary) {
+        self.metadata = asDataDict(dict)
+    }
+    init (_ src : CGImageSource) {
+        let d = CGImageSourceCopyPropertiesAtIndex(src,0, nil)
+        self.metadata=asDataDict(d)
     }
     
+    var asCFDict : CFDictionary? { metadata as CFDictionary? }
+    
+    var exifDictionary : DataDict {
+        get { asDataDict(metadata[Keys.TIFFDictionary.str]) }
+        set { metadata[Keys.TIFFDictionary.str] = newValue }
+    }
+    var jfifDictionary : DataDict {
+        get { asDataDict(metadata[Keys.JFIFDictionary.str]) }
+        set { metadata[Keys.JFIFDictionary.str] = newValue }
+    }
+    subscript(_ key : CFString) -> Any? {
+        get { metadata[key as String] }
+        set { metadata[key as String] = newValue }
+    }
 }
+
+
